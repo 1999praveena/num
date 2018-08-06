92e4c16349199129354cd708fdcad29ae60982bf
Praveena A M <praveenaarokiaraj@gmail.com>
2018-08-06 14:08:53 +0530

    Create Maineven.java

---
+++ Maineven.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class MainEvenOdd
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter num");
+a=sc.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
